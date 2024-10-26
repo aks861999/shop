1bb7efb52bb721e82f6463678eb1650b0fb727a8
Akash Biswas <biswas.ak.1999@gmail.com>
2024-10-27 01:20:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,19 @@ import uvicorn
 
 app = FastAPI()
 
-# Function to fetch fresh data from Google Sheets
-async def fetch_sheets_data():
+
+
+@app.get('/sweaters')
+async def get_sweaters():
+
+    '''
+    
     sheet_url = 'https://docs.google.com/spreadsheets/d/1pm2eBiux4l9Z-3P1eSAinP5NJ7v3v7YfpVFRfVW3rPA/gviz/tq?tqx=out:json&sheet=sweater'
     response = requests.get(sheet_url)
+    print(response.status_code)
+
+    data = []
+
     if response.status_code == 200:
         json_str = response.text.replace("/*O_o*/\ngoogle.visualization.Query.setResponse(", "")[:-2]
         data = json.loads(json_str)
@@ -22,14 +31,18 @@ async def fetch_sheets_data():
             }
             for row in data['table']['rows']
         ]
-        return records
-    return []
+        data = records
+    
 
-@app.get('/sweaters')
-async def get_sweaters():
-    data = await fetch_sheets_data()
+    print(data)
+
     if data:
         return JSONResponse(content=data)
     raise HTTPException(status_code=500, detail="Failed to fetch data")
 
 
+
+    
+    '''
+    return "Hello World"
+    
